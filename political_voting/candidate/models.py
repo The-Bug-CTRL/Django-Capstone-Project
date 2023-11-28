2e944021abc20d7ee7860ae2b77c923df5bc7d1e
Andrew <theandrewsargeant1@gmail.com>
2023-11-28 21:32:00 +0200

    Add docstrings for views and models

--- political_voting/candidate/models.py
+++ political_voting/candidate/models.py
@@ -3,10 +3,26 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Poll(models.Model):
+    """
+    Model representing a poll.
+
+    Fields:
+    - question (CharField): The question of the poll.
+    - option1 (CharField): The first option for the poll.
+    - option2 (CharField): The second option for the poll.
+
+    Methods:
+    - __str__(): Returns a string representation of the poll (question).
+
+    Usage:
+    - This model is used to represent polls in the application.
+    """
     question = models.CharField(max_length=200)
     option1 = models.CharField(max_length=100)
     option2 = models.CharField(max_length=100)
 
     def __str__(self):
+        """
+        Returns a string representation of the poll (question).
+        """
         return self.question
-
--- political_voting/candidate/views.py
+++ political_voting/candidate/views.py
@@ -1,4 +1,3 @@
-# candidate/views.py
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
@@ -10,6 +9,18 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
 def register(request):
+    """
+    View for user registration.
+
+    If the request method is POST, it processes the registration form.
+    If the form is valid, a new user is created, logged in, and redirected to the login page.
+    If the form is not valid, error messages are printed.
+
+    If the request method is GET, it renders the registration form.
+
+    :param request: HTTP request object.
+    :return: Rendered HTML template with the registration form.
+    """
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -25,8 +36,19 @@ def register(request):
     return render(request, 'candidate/registration/register.html', {'form': form})
 
 
-
 def user_login(request):
+    """
+    View for user login.
+
+    If the request method is POST, it processes the login form.
+    If the form is valid, the user is authenticated and logged in, then redirected to the home page.
+    If the form is not valid, an error message is added to the form.
+
+    If the request method is GET, it renders the login form.
+
+    :param request: HTTP request object.
+    :return: Rendered HTML template with the login form.
+    """
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -43,16 +65,47 @@ def user_login(request):
 
     return render(request, 'candidate/login.html', {'form': form})
 
+
 @login_required
 def home(request):
+    """
+    View for the home page.
+
+    Requires the user to be logged in.
+    Retrieves all polls and renders the home page.
+
+    :param request: HTTP request object.
+    :return: Rendered HTML template with the home page.
+    """
     polls = Poll.objects.all()
     return render(request, 'candidate/home.html', {'polls': polls})
 
+
 def page(request, poll_id):
+    """
+    View for displaying a specific poll page.
+
+    Retrieves the poll with the given poll_id and renders the corresponding page template.
+
+    :param request: HTTP request object.
+    :param poll_id: ID of the poll to display.
+    :return: Rendered HTML template for the specific poll page.
+    """
     poll = get_object_or_404(Poll, pk=poll_id)
     return render(request, f'candidate/page{poll_id}.html', {'poll': poll})
 
+
 def vote(request, poll_id):
+    """
+    View for processing user votes.
+
+    If the request method is POST, it processes the vote.
+    Validates the selected choice and redirects to the home page.
+
+    :param request: HTTP request object.
+    :param poll_id: ID of the poll for which the vote is being processed.
+    :return: Redirect to the home page or an error response.
+    """
     if request.method == 'POST':
         poll = get_object_or_404(Poll, pk=poll_id)
         selected_choice = request.POST.get('choice', None)
@@ -66,6 +119,16 @@ def vote(request, poll_id):
     else:
         return HttpResponse('Invalid request')
 
+
 def poll(request, poll_id):
+    """
+    View for displaying a specific poll.
+
+    Retrieves the poll with the given poll_id and renders the poll template.
+
+    :param request: HTTP request object.
+    :param poll_id: ID of the poll to display.
+    :return: Rendered HTML template for the specific poll.
+    """
     poll = get_object_or_404(Poll, pk=poll_id)
     return render(request, 'candidate/polls/poll.html', {'poll': poll})
